2e81c1a46f7eafee0b4975e3e1adc6178f7b212f
Luke Appleby <lukeappleb@gmail.com>
2022-05-06 13:52:59 -0300

    added kill switch feature, error handling for get request, error handling for email session login

--- ip.py
+++ ip.py
@@ -2,10 +2,10 @@ import logging
 import smtplib
 import sys
 import configparser
+import psutil as psutil
 from requests import get
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-
 
 # Setup properties file and logger
 p = configparser.RawConfigParser()
@@ -15,13 +15,29 @@ logging.basicConfig(filename=p.get('p', 'log_file'),
                     format='%(asctime)s,%(msecs)d %(name)s %(levelname)s %(message)s',
                     datefmt='%d-%b-%y %H:%M:%S',
                     level=logging.INFO)
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout)) # Set logger to output to console + file
+logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))  # Set logger to output to console + file
 
 testing = p.get('p', 'dev_mode')
 logging.info('testing=%s', testing)
 
-ip      = get('https://api.ipify.org').content.decode('utf8')
+ip_api_response = get('https://api.ipify.org')
+if ip_api_response.status_code != 200 or len(ip_api_response.content) > 100:
+    logging.error('IP API ERROR - ending script')
+    exit()
+
+ip = ip_api_response.content.decode('utf8')
 last_ip = open(p.get('p', 'last_ip'), 'r').readline()
+
+if p.get('p', 'use_kill_switch') == 'true':
+    logging.info('Kill switch ACTIVE')
+
+    if p.get('p', 'kill_switch_exe') in (i.name() for i in psutil.process_iter()):
+        logging.info('Kill switch exe found - ending script')
+        exit()
+    logging.info('Kill switch exe NOT found - proceeding')
+
+else:
+    logging.info('Kill switch NOT ACTIVE - proceeding')
 
 if ip != last_ip:
     logging.info('IP has been changed')
@@ -44,11 +60,22 @@ if ip != last_ip:
 
     logging.info('recipients=%s', recipients)
 
-    # Create SMTP session for sending the mail
-    session = smtplib.SMTP('smtp.gmail.com', 587)
-    session.starttls()  # enable security
-    session.login(p.get('Sender', 'email'),
-                  p.get('Sender', 'pass'))
+    # if we cant get logged in, log the error and change the IP back to previous
+    try:
+        # Create SMTP session for sending the mail
+        session = smtplib.SMTP('smtp.gmail.com', 587)
+        session.starttls()  # enable security
+        session.login(p.get('Sender', 'email'),
+                      p.get('Sender', 'pass'))
+    except:
+        logging.error('Unable to login to gmail account')
+
+        logging.info('resetting last_ip')
+        with open(p.get('p', 'last_ip'), 'w') as file:
+            file.write(last_ip)
+
+        logging.info('ending script')
+        exit()
 
     for recipient in recipients:
         message = MIMEMultipart()
